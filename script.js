1ba71be51d41f57638eebd53f746dcb141a550b9
KentaroSuzuki0405 <mahirocafe@gmail.com>
2023-01-20 20:12:10 +0900

    FirstCommit

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ var mySwiper02 = new Swiper ('.swiper-container02', {
       disableOnInteraction: false,
   }
 })
+
+// introスクロール
+const itemWrapper = document.querySelector('.side-scroll__itemOuter');
+const itemInner = document.querySelector('.top-intro__inner');
+
+gsap.to(itemInner, {
+x: () => -(itemInner.clientWidth - itemWrapper.clientWidth),
+ease: 'none',
+scrollTrigger: {
+    trigger: '.side-scroll__section', 
+    start: 'top top',
+    end: () => `+=${itemInner.clientWidth - itemWrapper.clientWidth}`,
+    // markers: true,
+    scrub: true, 
+    pin: true,  
+    invalidateOnRefresh: true, 
+    anticipatePin: 1, 
+},
+});
+
